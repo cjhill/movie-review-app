43c9c1f366d9001cd566538c64f865ce49111979
Chris Hill <cjhill0216@gmail.com>
2016-06-02 09:17:45 -0400

    Init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieReviewAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieReviewAppApp
+ */
+angular.module('movieReviewAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
